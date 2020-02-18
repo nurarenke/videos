d82e58959ece3e1418f38ee03db558a1ff00e948
Nura Renke <nurarenke@Nuras-MacBook-Pro.local>
2020-02-17 17:22:21 -0800

    Add SearchBar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  //term is short for search term
+  state = { term: "fsfsdfsdf" };
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" value={this.state.term}></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
