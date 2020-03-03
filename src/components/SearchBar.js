72a30a9d421f375d145b3f5888e1e7a138829021
Nura Renke <nurarenke@Nuras-MacBook-Pro.local>
2020-03-02 16:07:06 -0800

    Set up Search Bar Component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,29 @@ import React from "react";
 
 class SearchBar extends React.Component {
   //term is short for search term
-  state = { term: "fsfsdfsdf" };
+  state = { term: "" };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    //TODO: Make sure we call
+    // callback from parent component
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" value={this.state.term}></input>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
