e1172991c6bac3bf5842293f1e3b9b63672efe6a
Nura Renke <nurarenke@Nuras-MacBook-Pro.local>
2020-03-02 17:08:24 -0800

    Add YouTube Api

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const KEY = "AIzaSyBcdMxp6UkKC2yA79ID6wFYIe1Z4sByNzs";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: `${KEY}`
+  }
+});
